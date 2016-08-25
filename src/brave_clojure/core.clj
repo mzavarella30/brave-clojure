691b5d13dbc722c98811bc768bd8643e7ce6954b
Michael Zavarella <zavarella.dev@gmail.com>
2016-08-25 12:35:34 -0400

    Clojure basics and learning vim fireplace

--- src/brave_clojure/core.clj
+++ src/brave_clojure/core.clj
@@ -1,6 +1,35 @@
 (ns brave-clojure.core
   (:gen-class))
 
+;; Chapter 1 and 2 don't involved code
+
+;; Chapter 3 - Do Things: A Clojure Crash Course
+
+(+ 1 2 3) ;; 6
+(- 1 2 3) ;; -4
+ 
+(str "I am a panda" "," " and I want a taco!")
+
+(if true 1 0) ;; 1
+(if false 1 0) ;; 0
+
+(if true
+  (do (println "success")
+      1)
+  (do (println "fail")
+      -1))
+
+;; Using vim w/ fireplace 
+;; `cp` will evaluate a line
+;; `cpp` will evaluate the form which it is in 
+
+
+
+
+
+
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
