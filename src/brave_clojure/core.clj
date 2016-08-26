9d17c4f3a39fea7f6f4ba8444ffa5d366a1b4820
Michael Zavarella <zavarella.dev@gmail.com>
2016-08-26 08:58:43 -0400

    Made it through the hobbit thing and had to fix a ton of pseudocode

--- src/brave_clojure/core.clj
+++ src/brave_clojure/core.clj
@@ -1,5 +1,7 @@
 (ns brave-clojure.core
-  (:gen-class))
+  (:gen-class)
+  (use [clojure.string :as s]))
+
 
 ;; Chapter 1 and 2 don't involved code
 ;;;;;;;;;;;;
@@ -31,7 +33,7 @@
 ;; `cpp` will evaluate the form which it is in
 
 ;; not bothering with `when`, it's anti-fp
-
+   
 ;; nil and truthy/falsey-ness
 (nil? 1) ;; false
 (nil? nil) ;; true
@@ -160,7 +162,7 @@
 
 ;; you can also do stuff like so
 
-(:a #{:a :b}) ;; :a
+;; (:a #{:a :b}) ;; :a
 
 
 
@@ -176,7 +178,7 @@
 
 ;; Higher order functions allow for even more flexibility
 
-((or fn1 fn2) params) ;; (fn1 paarams) or (fn2 params)
+;; ((or fn1 fn2) params) ;; (fn1 paarams) or (fn2 params)
 
 (or + -) ;; returns `+`
 
@@ -200,9 +202,13 @@
 
 ;; Function, macro, and special forms 
 
-(if boolean
-  then-stuff
-  optional-else-stuff)
+;;(if boolean
+;;  then-stuff
+;;  optional-else-stuff)
+
+(def happy true)
+
+(defn say [a] (println a))
 
 (if happy
   (say "Im happy")
@@ -214,35 +220,116 @@
 
 (defn function
   "doc string for the function"
-  [params go here]
-  (str params go here "for the function"))
+  [p g h]
+  (str p g h "for the function"))
 
-(doc function) ;; returns the doc string
+;; (doc function) ;; returns the doc string
 
-(defn multiarity
-  ([p] (multiarity p 0))
-  ([p q] (str "mind your" p " and " q)))
+;;(defn multiarity
+;;  ([p] (multiarity p 0))
+;;  ([p q] (str "mind your" p " and " q)))
 
 ;; better than overloading a function
 
 
-(defn using-&
-  [a b c & xs]
-  (do-stuff-with a b c xs))
+;;(defn using-&
+;;  [a b c & xs]
+;;  (do-stuff-with a b c xs))
 
 ;; I'm not going to take notes on destructuring
 
 ;; The next section just talks about using stuff in functions
 
+;; It's worth mentioning that clojure has no privileged functions
+;; It's not that important really if you write simple code
+;; but the simplicity is kind of cool
 
+;; Anonymous Functions
 
+;; There are a few ways to define a function
 
+;; `defn` <- for defining actual functions
+;; (fn [] ...) <- for above AND anon functions
+;; #(... %1 %2 %3) <- for anon functions
 
+;;(def all-the-data)
 
+;;(defn mappy-mcmapface []
+;;  ...)
 
+;;(map mappy-mcmapface all-the-data)
 
+;;(map (fn [[a b c]] ...) all-the-data
+;; here the vector will be taken one-by-one from `all-the-data`
+;; use this for slightly beyond trivial stuff
 
+;;(map #(... %1) all-the-data)
+;; here the param %1 is taken from all-the-data directly
+;; use this for the stupidly simple
 
+;; Returning functions
+
+(defn inc-maker
+  "Create a custom incrementer"
+  [x]
+  #(+ % x))
+
+(def inc8 (inc-maker 8))
+
+(inc8 0) ;; 8
+
+;; Hobbit thing
+
+(def asym-bits
+  [{:name "head" :size 3}
+   {:name "left-ear" :size 1}
+   {:name "left-eye" :size 1}
+   {:name "mouth" :size 1}
+   {:name "nose" :size 1}
+   {:name "neck" :size 2}
+   {:name "left-shoulder" :size 3}
+   {:name "left-upper-arm" :size 3}
+   {:name "chest" :size 10}
+   {:name "back" :size 10}
+   {:name "left-forearm" :size 3}
+   {:name "abdomen" :size 6}
+   {:name "left-kidney" :size 1}
+   {:name "left-hand" :size 2}
+   {:name "left-knee" :size 2}
+   {:name "left-thigh" :size 3}
+   {:name "left-lower-leg" :size 4}
+   {:name "left-achilles" :size 1}
+   {:name "left-foot" :size 2}])
+
+(defn matching-part
+  "Creates a matching hobbit part"
+  [part]
+  {:name (s/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+(defn better-matching-part
+  "Use destructuring like you should"
+  [{:keys [name size]}]
+  {:name (s/replace p-name #"^left-" "right-")}
+   :size size)
+
+(defn symmetrize-body-parts
+  "Expects a seq of maps with :name and :size"
+  [asym-parts]
+  (loop [remaining-parts asym-parts
+         final-parts []]
+    (if (empty? remaining-parts)
+      final-parts
+      (let [[part & remaining] remaining-parts]
+        (recur remaining
+               (into final-parts
+                     (set [part (matching-part part)])))))))
+
+(symmetrize-body-parts asym-bits)
+
+;; I forgot that this was an acutal namespace 
+;; and was writing a ton of pseudo-code.
+;; Took me an hour to fix the damn thing
 
 
 
